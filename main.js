4d8139162ac27093e031a6a370e439fecb05b348
ivnenunes01 <ivne.vieira@aluno.senai.rs>
2025-08-29 17:11:16 -0300

    second commit

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
-// fetch("https://dog.ceo/dog-api")
-//    .then((response) => response.json())
-//    .then((data) => console.log(data))
-//    .catch((error) => console.error("erro ao buscar os dados"));
+// fetch('https://dog.ceo/api/breeds/image/random')
+//   .then(response => response.json())
+//   .then(data => {
+//     const img = document.getElementById('foto');
+//     img.src = data.message;
+//     img.alt = "Imagem de cachorro";
+//   })
+//   .catch(error => console.error('Erro ao carregar a imagem:', error));
+
+const btn = document.getElementById('btn-mudar');
+const img = document.getElementById('foto');
+
+let intervalo = null;
+let ativo = false;
+
+function trocarImagem() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+      img.src = data.message;
+      img.alt = "Imagem de cachorro";
+    })
+    .catch(error => console.error('Erro ao carregar a imagem:', error));
+}
+
+btn.addEventListener('click', () => {
+  if (!ativo) {
+    trocarImagem(); // primeira troca
+    intervalo = setInterval(trocarImagem, 2000); // começa a trocar
+    btn.textContent = "Parar Imagens";
+    ativo = true;
+  } else {
+    clearInterval(intervalo); // para a troca
+    btn.textContent = "Mudar Imagem";
+    ativo = false;s
+  }
+});
+
+
+
+
+
+
+
+       //    .catch((error) => console.error("erro ao buscar os dados"));
 
    //  async/await 
 
-   async function CarregarUsuarios(){
-    try {
-       let response = await fetch("https://dog.ceo/api/breeds/image/random");
-       let data = await response.json();
-       console.log(data);
-   }   catch (error) {
-       console.error("erro ao buscar os dados:", error);
-   }
-}
+//    async function CarregarUsuarios(){
+//     try {
+//        let response = await fetch("https://dog.ceo/api/breeds/image/random");
+//        let data = await response.json();
+//        console.log(data);
+//    }   catch (error) {
+//        console.error("erro ao buscar os dados:", error);
+//    }
+// }
 
-CarregarUsuarios()
+// CarregarUsuarios()
